ddc0acde999c85f361391815d0c4f3d7042c9e82
Audun Tråholt <audun@traholt.net>
2026-04-09 09:43:12 +0200

    add unoptimized.nix

---
+++ modules/unoptimized.nix
@@ -0,0 +1,58 @@
+{
+  config,
+  lib,
+  inputs,
+  ...
+}:
+let
+  nixpkgs-unoptimized = import inputs.nixpkgs {
+    system = "x86_64-linux";
+    config = config.nixpkgs.config;
+  };
+  nixpkgs-without-cuda = import inputs.nixpkgs {
+    system = "x86_64-linux";
+    config = config.nixpkgs.config // {
+      cudaSupport = false;
+    };
+  };
+  nixpkgs-without-rocm = import inputs.nixpkgs {
+    system = "x86_64-linux";
+    config = config.nixpkgs.config // {
+      rocmSupport = false;
+    };
+  };
+  useUnoptimized-x64 =
+    pkgList:
+    lib.lists.foldr (
+      name: acc:
+      (lib.attrsets.setAttrByPath [ name ] (
+        lib.attrsets.getAttrFromPath [ name ] nixpkgs-unoptimized.pkgs
+      ))
+      // acc
+    ) { } pkgList;
+
+  useUnoptimized-i686 =
+    pkgList:
+    lib.lists.foldr (
+      name: acc:
+      (lib.attrsets.setAttrByPath [ name ] (
+        lib.attrsets.getAttrFromPath [ name ] nixpkgs-unoptimized.pkgs.pkgsi686linux
+      ))
+      // acc
+    ) { } pkgList;
+in
+{
+  nixpkgs.overlays = [
+    (
+      final: prev:
+      if prev.stdenv.system == "x86_64-linux" then
+        (useUnoptimized-x64 [
+          # 20260403 - continues to fail optimised compilation after retesting
+          "mesa"
+          "assimp"
+        ])
+      else
+        (useUnoptimized-i686 [ ])
+    )
+  ];
+}
