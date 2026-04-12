0c0af2ab63746fc0b5ce57ed479e81df9e68cef5
Audun Tråholt <audun@traholt.net>
2026-04-12 02:30:36 +0200

    add dynamic setups to home.nix. all this sway and labwc shit has to go to their own files though, TODO

--- users/audun/home.nix
+++ users/audun/home.nix
@@ -1,27 +1,61 @@
 {
+  lib,
   pkgs,
   inputs,
+  myLib,
   ...
 }:
+
 {
   home-manager.users.audun = {
     imports = [
       inputs.catppuccin.homeModules.catppuccin
       ../../modules/btop.nix
-      ../../modules/ghostty.nix
-      ../../modules/mangohud.nix
       #../../modules/nushell.nix
-      ../../modules/vesktop.nix
-      ../../modules/vivaldi.nix
-      ../../modules/vscode.nix
-    ];
+    ]
+    ++
+      lib.optional
+        (myLib.hasAllTags [
+          "gui"
+          "nixos"
+        ])
+        [
+          ../../modules/mangohud.nix
+          ../../modules/plasma.nix
+        ]
+    ++
+      lib.optional
+        (myLib.hasAnyTag [
+          "gui"
+          "darwin"
+        ])
+        [
+          ../../modules/ghostty.nix
+          ../../modules/vesktop.nix
+          ../../modules/vivaldi.nix
+          ../../modules/vscode.nix
+        ];
     catppuccin = {
       enable = true;
       flavor = "mocha";
       bat.enable = true;
       eza.enable = true;
       zellij.enable = true;
+      mangohud.enable = false;
+      sway.enable = false;
     };
+    wayland.windowManager.labwc = {
+      enable = true;
+      xwayland.enable = true;
+    };
+    home.packages = with pkgs; [
+      labwc # The floating compositor
+      foot # Terminal
+      dmenu-wayland # App launcher
+      swaylock
+      swayidle
+      waybar # Taskbar/Clock
+    ];
     home.shell.enableShellIntegration = true;
     home.homeDirectory = if pkgs.stdenv.hostPlatform.isDarwin then "/Users/audun" else "/home/audun";
     home.stateVersion = "26.05";
